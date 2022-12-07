fc93287e3fb207548c9f1a35fbfec5bcc825ac40
Aldo Martinez <aavalosmt@gmail.com>
2022-12-06 22:26:08 -0600

    Add pokemon descriptions

--- detail.js
+++ detail.js
@@ -13,11 +13,15 @@ async function getDetail(name) {
     const levelMovements = $('#tab-moves-21 > div > div:nth-child(1) > div:nth-child(3) > .data-table > tbody > tr')
     const eggMovements = $('#tab-moves-21 > div > div:nth-child(1) > div:nth-child(6) > .data-table > tbody > tr')
     const tmMovements = $('#tab-moves-21 > div > div:nth-child(2) > div > .data-table > tbody > tr')
+    const stats = $('.sv-tabs-panel > div:nth-child(2) > div.grid-col.span-md-12.span-lg-8 > div.resp-scroll > .vitals-table > tbody > tr')
+    const descriptions = $('#main > div:nth-child(16) > .vitals-table > tbody > tr')
 
     data["attributes"] = parseAttributes($, attributes)
+    data["stats"] = parseStats($, stats)
     data["level-movements"] = parseMovements($, levelMovements)
     data["egg-movements"] = parseMovements($, eggMovements)
     data["tm-movements"] = parseMovements($, tmMovements)
+    data["descriptions"] = parseDescriptions($, descriptions)
 
     return data
 }
@@ -29,7 +33,7 @@ function parseAttributes($, attributes) {
         const key = $(el).find('th').text()
         const td = $(el).find('td')        
         const values = []
-        
+
         if(i < (attributes.length - 1)) {
             var iterate = function (parent, result = []) {
                 let children = $(parent).children();
@@ -70,6 +74,48 @@ function parseAttributes($, attributes) {
         data[key] = values
     })
 
+    return data
+}
+
+function parseDescriptions($, descriptions) {
+    var data = []
+
+    descriptions.each((i, el) => {
+        var gameData = []
+        const games = $(el).find('th')
+        const description = $(el).find('td').text()
+
+        games.children().each((i, el) => {
+            const text = $(el).text()
+            if(text != '') {
+                gameData.push(text)
+            }
+        })
+
+        data.push({
+            games: gameData,
+            description: description
+        })
+    })
+    return data
+}
+
+function parseStats($, stats) {
+    var data = {}
+
+    stats.each((i, el) => {
+        const numValues = $(el).find('.cell-num')
+        const key = $(el).find('th').text()
+        const base = $(numValues.get(0)).text()
+        const min = $(numValues.get(1)).text()
+        const max = $(numValues.get(2)).text()
+
+        data[key] = {
+            base: base,
+            min: min,
+            max: max
+        }
+    })
     return data
 }
 
